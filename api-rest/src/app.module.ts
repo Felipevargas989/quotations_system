90993b250a0c1df8107a27105f7d7a9e39b57632
leoBravo <leo.bravo.rain@gmail.com>
2025-09-11 19:33:51 -0300

    feat: create users resource

--- api-rest/src/app.module.ts
+++ api-rest/src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { SupabaseModule } from './supabase/supabase.module';
 import { CompaniesModule } from './companies/companies.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { CompaniesModule } from './companies/companies.module';
     ConfigModule,
     SupabaseModule,
     CompaniesModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
