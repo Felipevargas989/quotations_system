abf08cfd00647cb34d86184f3f777009a4f84f8b
leoBravo <leo.bravo.rain@gmail.com>
2025-09-12 11:58:30 -0300

    refactor: move tests to tests folders

--- api-rest/src/clients/tests/clients.service.spec.ts
+++ api-rest/src/clients/tests/clients.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CompaniesService } from './companies.service';
+import { ClientsService } from '../clients.service';
 
-describe('CompaniesService', () => {
-  let service: CompaniesService;
+describe('ClientsService', () => {
+  let service: ClientsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CompaniesService],
+      providers: [ClientsService],
     }).compile();
 
-    service = module.get<CompaniesService>(CompaniesService);
+    service = module.get<ClientsService>(ClientsService);
   });
 
   it('should be defined', () => {
