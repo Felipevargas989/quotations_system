dbd4a1fb8c67382f4bd75228a6f89c556ea91bba
leoBravo <leo.bravo.rain@gmail.com>
2025-10-17 19:56:00 -0300

    feat(api): create module to manage email notifiaciones. Add service to send email

--- api-rest/src/email/email.service.ts
+++ api-rest/src/email/email.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PinoLogger } from 'nestjs-pino';
+import { Resend } from 'resend';
 
 @Injectable()
 export class EmailService {
@@ -13,7 +14,7 @@ export class EmailService {
     const resend = new Resend(
       this.configService.get<string>('RESEND_API_KEY') as string,
     );
-    this.logger.log(`Sending email to ${to} with subject ${subject}`);
+    this.logger.info(`Sending email to ${to} with subject ${subject}`);
     await resend.emails.send({
       from: 'Eventia <hola@eventi-app.com>',
       to: [to],
