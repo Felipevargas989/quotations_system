9b4972aad7455273de5617d48f9fbf4d2e503f86
leoBravo <leo.bravo.rain@gmail.com>
2025-10-27 15:03:02 -0300

    feat(front): render responses from survey

--- frontend/src/types/customerSatisfactionSurveys.types.ts
+++ frontend/src/types/customerSatisfactionSurveys.types.ts
@@ -1,3 +1,6 @@
+import { Client } from "./clients.types";
+import { Quotation } from "./quotations.types";
+
 export interface Question {
   id: number;
   question: string;
@@ -21,3 +24,16 @@ export interface CreateAnswerDto {
   quotationId: string;
   answers: Answer[];
 }
+
+export type CustomerSatisfactionSurveyResponse = {
+  id: number;
+  created_at: Date;
+  quotation_id: Quotation["id"];
+  template_id: string;
+  answers: Answer[];
+  quotations: Pick<
+    Quotation,
+    "id" | "quotation_number" | "event_date" | "event_type" | "company_id"
+  > & { clients: Pick<Client, "name"> };
+  templates: SurveyTemplate;
+};
