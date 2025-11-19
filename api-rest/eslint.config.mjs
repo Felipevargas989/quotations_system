4cc76655ff6261f19dc24bbe765b78c6e51a36e7
leoBravo <leo.bravo.rain@gmail.com>
2025-11-19 12:58:51 -0300

    test(quotation): add test when quotation status is differnet from ACEPTADA and ENVIADA

--- api-rest/eslint.config.mjs
+++ api-rest/eslint.config.mjs
@@ -33,4 +33,10 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-calls': 'off',
     },
   },
+  {
+    files: ['**/*.spec.ts', '**/*.test.ts'],
+    rules: {
+      '@typescript-eslint/unbound-method': 'off',
+    },
+  },
 );
--- api-rest/src/quotations/tests/unit/quotations.service.spec.ts
+++ api-rest/src/quotations/tests/unit/quotations.service.spec.ts
@@ -3,6 +3,8 @@ import { PinoLogger } from 'nestjs-pino';
 import { ClientsService } from 'src/clients/clients.service';
 import { EmailService } from 'src/email/email.service';
 import { PaymentsService } from 'src/payments/payments.service';
+import { QuotationStatus } from 'src/quotations/constants/constants';
+import { UpdateQuotationDto } from 'src/quotations/dto/update-quotation.dto';
 import { QuotationsRepository } from 'src/quotations/quotations.repository';
 import { RefundsService } from 'src/refunds/refunds.service';
 import { UsersService } from 'src/users/users.service';
@@ -27,7 +29,7 @@ describe('QuotationsService', () => {
       findOne: jest.fn(),
       // findAll: jest.fn(),
       // create: jest.fn(),
-      // update: jest.fn(),
+      update: jest.fn(),
       // delete: jest.fn(),
     } as any;
 
@@ -130,6 +132,31 @@ describe('QuotationsService', () => {
 
       await expect(service.update('1', {}, 1)).rejects.toThrow(
         'Quotation not found',
+      );
+    });
+
+    it('when quotation status is differente from ACEPTADA and ENVIADA, it should execute the repo.update function', async () => {
+      quotationsRepositoryMock.findOne.mockResolvedValue({
+        data: {
+          quotation_status: QuotationStatus.EN_NEGOCIACION,
+        },
+        error: null,
+      });
+
+      const params: UpdateQuotationDto = {
+        quotation_status: QuotationStatus.EN_NEGOCIACION,
+      };
+
+      const quotation_id = '1';
+      const company_id = 1;
+
+      await service.update(quotation_id, params, company_id);
+
+      // service.update.
+      expect(quotationsRepositoryMock.update).toHaveBeenCalledWith(
+        quotation_id,
+        params,
+        company_id,
       );
     });
   });
